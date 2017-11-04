77835817943ec52e0157440337cea1563aabff85
Joey Biotti <joey@MacBook-Pro-2.local>
2017-11-04 18:11:48 -0500

    added react functions

--- src/app.jsx
+++ src/app.jsx
@@ -7,3 +7,23 @@ var query = '';
 var cities = [];
 var citiesWeather = [];
 var currentCity = 0;
+
+var Weather = React.createClass({
+    render: function() {
+
+    }
+});
+
+// Assign the React comps to DOM Element
+var element = React.createElement(Weather, {});
+ReactDOM.render(element, document.querySelector('.container'));
+
+//Init data for UI
+getInitialState: function() {
+    return {
+        weather: '',
+        temp: 0,
+        humidity: 0,
+        wind: 0
+    }
+},
