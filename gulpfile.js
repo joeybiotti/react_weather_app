dabb19cbc4858e7f8f1dd7a42dae655c81a1df64
Joey Biotti <joey@MacBook-Pro-2.local>
2017-11-04 17:32:23 -0500

    gulp error messages

--- gulpfile.js
+++ gulpfile.js
@@ -10,3 +10,27 @@ var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 
+    // format error messages
+var notify = function(error) {
+    var message = 'In: ' ;
+    var title = 'Error: ';
+
+    if(error.description){
+        title += error.description;
+    }
+
+    else if (error.message) {
+        title += error.message;
+    }
+
+    if (error.filename) {
+        var file = error.filename.split('/');
+        message += file[file.length-1];
+    }
+
+    if (error.lineNumber) {
+        message += '\nOn Line: ' + error.lineNumber;
+    }
+
+    notifier.notify({title: title, message: message});
+};
