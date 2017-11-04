ae475b865a08c22657770e82420b361e8aca04f0
Joey Biotti <joey@MacBook-Pro-2.local>
2017-11-04 18:06:46 -0500

    added city weather vars

--- src/app.jsx
+++ src/app.jsx
@@ -2,3 +2,8 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var classNames = require('classnames');
 var Api = require('./utils/api');
+
+var query = '';
+var cities = [];
+var citiesWeather = [];
+var currentCity = 0;
